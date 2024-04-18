6efdd78b56d2b46e6e3f3784b5ba620c4baf299d
Yogi Bagus <yogibaguskarunia@gmail.com>
2024-04-18 23:39:33 +0700

    Update package.json to add express dependency and server.js to initialize an Express.js server

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+// Initialize a WebSocket server
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8090 });
 
+// Initialize an Express.js server
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+// Get the IP address of the server
 const os = require('os');
 const ifaces = os.networkInterfaces();
 let ipAddress = '';
@@ -14,7 +22,16 @@ Object.keys(ifaces).forEach((ifname) => {
 console.log(`Server IP address: ${ipAddress}`);
 console.log('Server started on port 8090');
 
-// Listen for new connections
+// Serve the index.html file
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+// Listen for new socket connections
 wss.on('connection', (ws) => {
   console.log('Client connected');
   // Listen for messages from clients
