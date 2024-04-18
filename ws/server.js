77d1841de4213ffca294ce05e99b23457b84fdba
Yogi Bagus <yogibaguskarunia@gmail.com>
2024-04-19 00:57:17 +0700

    update

--- ws/server.js
+++ ws/server.js
@@ -1,56 +1,57 @@
-// Initialize a WebSocket server
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 8090 });
-
 // Initialize an Express.js server
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
+require('dotenv').config()
+var express = require('express');
+var app = express();
+
+//import the express-ws library and set up WebSocket support on the app instance.
+var expressWs = require('express-ws')(app);
 
 // Get the IP address of the server
 const os = require('os');
 const ifaces = os.networkInterfaces();
 let ipAddress = '';
 Object.keys(ifaces).forEach((ifname) => {
-  ifaces[ifname].forEach((iface) => {
-    if (iface.family === 'IPv4' && !iface.internal) {
-      ipAddress = iface.address;
-    }
-  });
+    ifaces[ifname].forEach((iface) => {
+        if (iface.family === 'IPv4' && !iface.internal) {
+            ipAddress = iface.address;
+        }
+    });
 });
 console.log(`Server IP address: ${ipAddress}`);
-console.log('Server started on port 8090');
+console.log(`Server started on port ${process.env.PORT}`);
 
 const path = require('path');
 // Serve the index.html file
 app.get('/', (req, res) => {
-  const indexPath = path.join(__dirname, '..', 'index.html');
-  res.sendFile(indexPath);
+    const indexPath = path.join(__dirname, '..', 'index.html');
+    res.sendFile(indexPath);
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+// 'aWss' holds the WebSocketServer object for further configuration and handling.
+var aWss = expressWs.getWss('/');
 
 // Listen for new socket connections
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  // Listen for messages from clients
-  ws.on('message', (message, isBinary) => {
-    // log client from which the message was received
-    idMessage = ws._socket.remoteAddress + ":" + ws._socket.remotePort;
-    console.log(`Received message from [${idMessage}]: ${message}`);
-    // Broadcast the received message to all connected clients
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        console.log(`Broadcasting message to [${client._socket.remoteAddress}:${client._socket.remotePort}] with message: ${message}`);
-        client.send(message, { binary: isBinary });
-      }
+app.ws('/ws', function (ws, req) {
+    console.log('Client connected');
+    // Listen for messages from clients
+    ws.on('message', (message, isBinary) => {
+        // log client from which the message was received
+        idMessage = ws._socket.remoteAddress + ":" + ws._socket.remotePort;
+        console.log(`Received message from [${idMessage}]: ${message}`);
+        // Broadcast the received message to all connected clients
+        aWss.clients.forEach(function (client) {
+            if (client !== ws && client.readyState === ws.OPEN) {
+                client.send(message)
+            }
+        });
+
     });
-  });
-  // Handle client disconnect
-  ws.on('close', () => {
-    console.log('Client disconnected');
-  });
+    // Handle client disconnect
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`listening on *:${process.env.PORT}`)
 });
